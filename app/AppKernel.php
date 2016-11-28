b59ab2960a069800902d01cd48c9a9ce77ec65b2
Jeremy Teyssedre <jteyssedre@atecna.fr>
2016-11-28 12:28:43 +0100

    add CatalogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Formation\PageBundle\PageBundle(),
+            new Formation\CatalogBundle\CatalogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
