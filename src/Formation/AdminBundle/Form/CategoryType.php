496bbb71ae53f1e65fb16f65f6f39e6fc47d557d
Jeremy Teyssedre <jteyssedre@atecna.fr>
2016-11-29 12:32:39 +0100

    add crud for category

---
+++ src/Formation/AdminBundle/Form/CategoryType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Formation\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Formation\CatalogBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'formation_adminbundle_category';
+    }
+
+
+}
